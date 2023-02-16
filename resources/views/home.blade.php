124fa52344af0d00b89abc3d929388f0ad6b9e70
angeloendonela2015 <angeloendonela@gmail.com>
2023-02-16 11:07:20 +0800

    dohmaster

---
+++ resources/views/home.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+      <div class="col-3 p-5">
+        <img src="doh-logo.png" class="rounded-circle" style="max-height:90px;">
+      </div>
+      <div class="col-9 py-5">
+        <div class="">
+          <h1>DOH Bicol CHD</h1>
+        </div>
+        <div class="d-flex">
+          <div class="pe-5">
+            <strong>153 </strong>posts
+          </div>
+          <div class="pe-5">
+            <strong>23k </strong>followers
+          </div>
+          <div class="pe-5">
+            <strong>212 </strong>following
+          </div>
+        </div>
+        <div class="pt-4 fw-bold">
+          doh.gov.ph
+        </div>
+        <div class="fw-lighter">
+          February 15 is International Childhood Cancer Day! Every year, International Childhood Cancer Day is observed to raise public awareness and express support for programs for children affected by cancer.
+        </div>
+        <div class="">
+          <a href="#">www.facebook.com/dohbicol</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-4">
+        <img src="https://scontent.flgp1-1.fna.fbcdn.net/v/t39.30808-6/331062698_1138836616803014_1525854244405847271_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeG9N1JWbE5cK0sWdNFyYxjd3h3y0PpwzkXeHfLQ-nDORenHkEdO60cZ2rzGwiZRLPeYfPZunY6--WsjoMzC6A6z&_nc_ohc=LMbXf-Dva1kAX_weBoa&_nc_ht=scontent.flgp1-1.fna&oh=00_AfCidlYfns3vaMLotnUOpYNq1qMgtQtzn-E2LxM_6ZeoBA&oe=63F27860" class="w-100">
+      </div>
+      <div class="col-4">
+        <img src="https://scontent.flgp1-1.fna.fbcdn.net/v/t39.30808-6/329146013_3396814040588346_7036628807404312321_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGKn0cclnph9MuJIIOss3HZFeL6BflpeDQV4voF-Wl4NL4q_O8Ugm0U36dx3oUyhuPO37Ji8x5n51BbdB1-8WSH&_nc_ohc=ZB-u_RY4jlIAX-QdDt-&_nc_oc=AQmhUiMdx2rvZgnTvVRny6HjdClLWPo7WyKuu7YpCWkxMxzYAFvDEK8LUXdNB0x-Pjc&_nc_ht=scontent.flgp1-1.fna&oh=00_AfCHHm1lmDVrKLbBWuIduTFHmqyOjNvcnt-QmoPn8iIGAg&oe=63F2BF63" class="w-100">
+      </div>
+      <div class="col-4">
+        <img src="https://scontent.flgp1-1.fna.fbcdn.net/v/t39.30808-6/330960689_813824920229184_552601289019195322_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeH1g7VnPFSqebjiKSWr2-uHngTzdJdJPayeBPN0l0k9rO49dDPWoA3c_bkEDFu0_MAJUO3Lup_GU6TqvxI5904x&_nc_ohc=pblJCxpppDwAX-l_yO2&_nc_ht=scontent.flgp1-1.fna&oh=00_AfDOSfBf4p_Wa0NT-d9sc1WPcofagO7I0ubmwX-J4FA-0A&oe=63F1F8FA" class="w-100">
+      </div>
+    </div>
+</div>
+@endsection
